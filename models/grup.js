e799e3ccdecb11ed80e9486f44454ea5cb187bb5
erga916 <erga916@vidalibarraquer.net>
2023-02-21 08:24:56 +0100

    TIPO DE GRUPO FIXEADO

--- models/grup.js
+++ models/grup.js
@@ -5,7 +5,11 @@ var Schema = mongoose.Schema;
 
 var GrupSchema = new Schema({
     nom: { type: String, required: true },
-    tipus: { type: String, required: true },
+    tipus: { type: String, required: true, 
+      enum: [
+      "Públic","Privat"     
+    ],
+    default: "Privat" },
     membres: [{ type: Schema.ObjectId, ref: "User" }]
     //usamos el nombre de la posición/objeto del esquema para listar el contenido en la view
     //usaremos membres para las funciones del controller
